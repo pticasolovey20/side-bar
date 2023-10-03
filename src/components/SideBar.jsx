dcbe56226dde58a1fcd8e00e5dc392be92707b81
unknown <dy.yev.solovyov@gmail.com>
2023-10-03 20:35:12 +0300

    v.1.2

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -11,7 +11,6 @@ import Wallet from "../icons/Wallet";
 import SideHeader from "./SideHeader";
 import SideTrigger from "./SideTrigger";
 import SideSearch from "./SideSearch";
-import SideSwitch from "./SideSwitch";
 import SideLogOut from "./SideLogOut";
 
 const NAVIGATION = [
@@ -31,7 +30,7 @@ const SideBar = () => {
 		<section
 			className={classNames(
 				isOpen ? "w-[250px]" : "w-[72px]",
-				"flex flex-col gap-10",
+				"fixed flex flex-col gap-10",
 				"rounded-br-lg text-text-color",
 				"bg-sidebar-color shadow-[0px_7px_10px] shadow-text-color/50",
 				"transition-width duration-500 ease-in-out"
@@ -43,7 +42,7 @@ const SideBar = () => {
 			</div>
 
 			<div className="flex flex-col h-full justify-between">
-				<div className="flex flex-col gap-2 px-4 py-2.5 pb-6 border-b">
+				<div className="flex flex-col gap-2 px-4 py-2.5 pb-4 border-b">
 					<SideSearch isOpen={isOpen} setIsOpen={setIsOpen} />
 					<nav>
 						<ul className="flex flex-col gap-2">
@@ -55,7 +54,7 @@ const SideBar = () => {
 										"p-2 rounded-md shadow-none cursor-pointer",
 
 										isActive === id
-											? "text-white bg-primary-color shadow-text-color shadow-md fill-white"
+											? "text-white bg-primary-color !shadow-text-color !shadow-md fill-white"
 											: "",
 
 										"fill-[#707070] hover:fill-white",
@@ -80,9 +79,8 @@ const SideBar = () => {
 					</nav>
 				</div>
 
-				<div className="flex flex-col gap-2 px-4 py-6">
+				<div className="p-4">
 					<SideLogOut isOpen={isOpen} />
-					<SideSwitch isOpen={isOpen} />
 				</div>
 			</div>
 		</section>
